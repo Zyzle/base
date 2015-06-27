b11e53ebae0c8ec1dfc7394088ba5a307f98d2b8
Zyzle <n1l0c2501@gmail.com>
2015-06-27 01:58:23 +0100

    adding in working router config, still not sure if issues are mine or angulars though

--- base.ts
+++ base.ts
@@ -1,15 +1,45 @@
-import {Component, View, bootstrap} from 'angular2/angular2';
-import {Router} from 'angular2/router';
+import {NgFor, Component, View, bootstrap, bind, Injectable} from 'angular2/angular2';
+import {Router, RouteConfig, RouterLink, RouterOutlet, routerInjectables} from 'angular2/router';
 
+@Component({
+  selector: 'other-page'
+})
+@View({
+  templateUrl: 'templates/other-page.html'
+})
+class OtherPage {
+
+}
+
+@Component({
+  selector: 'home-page'
+})
+@View({
+  templateUrl: 'templates/home-page.html'
+})
+class HomePage {
+
+}
+
+@RouteConfig([
+  { path: "/", redirectTo: '/home'},
+  { path: "/home", as: "home", component: HomePage },
+  { path: "/other", as: "other", component: OtherPage }
+])
 @Component({
   selector: 'base-app'
 })
 @View({
-  template: `<h1>HELLO WORLD!!!</h1>`
+  directives: [RouterOutlet, RouterLink],
+  templateUrl: 'templates/base-app.html'
 })
 class BaseApp{
 
 }
 
 
-bootstrap(BaseApp);
+bootstrap(BaseApp, [routerInjectables])
+  .then(
+    success => console.log(success),
+    error => console.log(error)
+  );
