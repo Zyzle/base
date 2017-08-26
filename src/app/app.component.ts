eb52efd20692ffb1865afe2ff18531122b3afa1d
Colin McCulloch <n1l0c2501@gmail.com>
2017-08-26 17:47:44 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'base-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'base';
+}
